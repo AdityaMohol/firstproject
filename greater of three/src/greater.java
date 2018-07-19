233913d927c6a5f4b4abbe8827bd85c9a517de78
Adi <Adi@192.168.0.2>
2018-07-19 02:24:47 -0700

    greater of three greater of three

---
+++ greater of three/src/greater.java
@@ -0,0 +1,26 @@
+
+public class greater 
+	{
+	 public static void main(String args[])
+	 {
+	   
+	   int a=1;
+	   int b=3;
+	   int c=4;
+	   {
+	    if (a>b && a>c)
+	  
+	      System.out.println(" A is greater ");
+	        
+	     else if (b>a && b>c)
+	      System.out.println(" A is greater ");
+	            
+	      else 
+	             
+	      System.out.println(" c is greater "); 
+	    }         
+	         
+	   }
+	}
+
+}
