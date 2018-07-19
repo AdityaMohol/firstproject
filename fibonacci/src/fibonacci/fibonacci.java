24da0a0c2b8a49ab7e24a4d517b86467259dfaa6
AdityaMohol <41040589+AdityaMohol@users.noreply.github.com>
2018-07-19 02:32:59 -0700

    Update fibonacci.java

--- fibonacci/src/fibonacci/fibonacci.java
+++ fibonacci/src/fibonacci/fibonacci.java
@@ -1,5 +1,20 @@
 package fibonacci;
 
 public class fibonacci {
-
+   public static void main(String args[])
+   {
+	   
+           int i,c,a=0,b=1;
+	   System.out.print(a+""+b);
+	   
+	   for(i=1;i<=10;i++)
+	   {
+		   c=a+b;
+		   System.out.print(""+c);
+		   a=b;
+		   b=c;
+	   }
+  
+	}
+}
 }
